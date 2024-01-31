1fa1dc4083278b3d266c483ab051056109331906
MariGsp <marygt182@gmail.com>
2024-01-31 14:56:04 +0000

    Add functionality to button

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,10 +8,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val responseLiveData = MutableLiveData<String>()
+    private val httpClient = HttpClient(Android)
+
+    private suspend fun fetchContent(): String {
+        val url =
+            "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json"
+        return httpClient
+            .get(url)
+            .bodyAsText()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,14 +39,21 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    val responseState = responseLiveData.observeAsState("").value
                     Column {
                         Button(
                             onClick = {
-
+                                lifecycleScope.launch {
+                                    val response = fetchContent()
+                                    runOnUiThread{
+                                        responseLiveData.value = response
+                                    }
+                                }
                             }
                         ) {
                             Text(text = "Download")
                         }
+                        Text(text = responseState.toString())
                     }
                 }
             }
